9e5e7665831d061b1c7199ddc880cd4a4d1b2ed7
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-19 14:33:34 +0530

    Braille & English-to-Braille

---
+++ Braille/Braille/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Braille
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RegularTextBox = new System.Windows.Forms.TextBox();
+            this.BrailleTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RegularTextBox
+            // 
+            this.RegularTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegularTextBox.Location = new System.Drawing.Point(12, 34);
+            this.RegularTextBox.Multiline = true;
+            this.RegularTextBox.Name = "RegularTextBox";
+            this.RegularTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.RegularTextBox.Size = new System.Drawing.Size(465, 154);
+            this.RegularTextBox.TabIndex = 0;
+            // 
+            // BrailleTextBox
+            // 
+            this.BrailleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrailleTextBox.ForeColor = System.Drawing.Color.Red;
+            this.BrailleTextBox.Location = new System.Drawing.Point(12, 227);
+            this.BrailleTextBox.Multiline = true;
+            this.BrailleTextBox.Name = "BrailleTextBox";
+            this.BrailleTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.BrailleTextBox.Size = new System.Drawing.Size(465, 154);
+            this.BrailleTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter The Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(204, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Converted to Braille";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 456);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BrailleTextBox);
+            this.Controls.Add(this.RegularTextBox);
+            this.Name = "Form1";
+            this.Text = "Braille Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox RegularTextBox;
+        private System.Windows.Forms.TextBox BrailleTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
